ff1671a579d02d02ba1d02a142f13e927f0f6660
Zoé Montbroussous <98592287+ZuluOscarUniform@users.noreply.github.com>
2023-01-08 19:00:05 +0100

    Cleanup main

--- functions/Heavyside_eta.m
+++
@@ -1,3 +0,0 @@
-%Approximation of the dirac function
-function [M] = Heavyside_eta(phi,eta)
-M = (1.+2.*atan(phi./eta)/pi)./2.;
--- functions/delta_eta.m
+++
@@ -1,3 +0,0 @@
-%Approximation of the dirac function
-function [M] = delta_eta(phi,eta)
-M = 1./((1 + (phi/eta).^2)*eta*pi);
